9f7f85e7b99403ccd5a99173780cab1ff43d7bc3
Staf Verhaegen <staf@fibraservi.eu>
2025-10-24 16:59:52 +0200

    LVS update
    * With feedback from Mattias updated the LVS deck patch so the `cheat`
      usage is only needed around `mos_extraction.lvs`.
    * Use separate net for nwell and substrate in cells where they are not
      contacted by taps. This allow to remove the use `connect_global` in the
      patched LVS deck. It does need a `connect_implicit` for the `nwell` net
      and also labels on the NWell so KLayout knows the net of the floating
      NWell regions.
      File `SRAMBlockTest_filled_M2fix_nwelllabels.gds` has been added but
      the only difference with `SRAMBlockTest_filled_M2fix.gds` is the added
      nwell labels; no design changes have been done.
    * Update the dual port netlist to match to wrong connection of the
      word lines. Row decoder of port1 is connected to wl2[*] and the one of
      port 2 to wl1[*]. Consequence is that the two ports can't be operated
      asynchronously. Will update documentation in a follow-up patch.

--- FlowSpace_SRAMBlocksTest/design_data/lvs/lvs.sh
+++ FlowSpace_SRAMBlocksTest/design_data/lvs/lvs.sh
@@ -12,8 +12,8 @@ mkdir ${d_run}
 d_pdk=`realpath /home/verhaegs/eda/Chipflow/code/c4m-arrakeen/PDKs/c4m-pdk-ihpsg13g2/deps/IHP-Open-PDK`
 cmd="${d_pdk}/ihp-sg13g2/libs.tech/klayout/tech/lvs/run_lvs.py"
 
-f_gds=`realpath ../gds/SRAMBlocksTest_filled_M2fix.gds`
-f_spi=`realpath ../spice/SRAMBlocksTest_nopadring_hack.spi`
+f_gds=`realpath ../gds/SRAMBlocksTest_filled_M2fix_nwelllabels.gds`
+f_spi=`realpath ./lvs_wrapper.spi`
 
 python3 $cmd --run_dir=${d_run} \
  --layout=${f_gds} --netlist=${f_spi} \
